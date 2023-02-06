c339946d098f6821c3cbf1128b662cd80fce0e2c
tetsuyainfra <tetsuyainfra@gmail.com>
2023-02-06 12:55:21 +0900

    init GatsbyJS

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const IndexPage = () => {
+  return (
+    <Layout pageTitle="Home Page">
+      <p>I'm making this by following the Gatsby Tutorial.</p>
+    </Layout>
+  )
+}
+
+export const Head = () => <title>Home Page</title>
+
+export default IndexPage
